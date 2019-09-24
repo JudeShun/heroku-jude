791463f531994f9baa9d4c968c16ef422c6c4baa
nanitkatipunan <nanit.katipunan@student.passerellesnumeriques.org>
2019-09-24 17:54:26 +0800

    i love chickboy

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+var userCount = 0; 
 
- var app = require('express')();
- var http = require('http').createServer(app);
- var port = process.env.PORT || 3000
- 
- app.get('/', function(req, res){
+
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
- });
- 
- http.listen(port, function(){
-  console.log('listening on *: ' + port);
- });
+});
+
+
+io.on('connection', socket => {
+  socket.on('chatmessage', function (msg) {
+    io.emit('chatmessage', msg);
+  })
+
+  // userCount++;
+	// io.sockets.emit('userCount', { userCount: userCount });
+	// socket.on('disconnect', function () {
+	// 	userCount--;
+	// 	io.sockets.emit('userCount', { userCount: userCount });
+	// });
+
+  socket.on('newuser', function(name){
+    socket.emit('newuser', name);
+  })
+
+  socket.on('userconnected', function(name){
+    socket.broadcast.emit('userconnected', name);
+  })
+});
+
+
+	// socket.on('userCount', function (data) {
+	// 	console.log(data.userCount);
+	// 	userCount = data.userCount;
+	// 	$('#user').text(userCount);
+	// });
+
+
+
+
+http.listen(port, function () {
+  console.log('listening on *:' + port);
+});
